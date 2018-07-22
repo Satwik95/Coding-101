a8f372804f220c3238600f92abbda8e885653f83
Satwik <satwik.mishra1995@gmail.com>
2018-07-23 01:32:25 +0530

    initial commit

---
+++ Competative Concepts/optimal_game_strategy.py
@@ -0,0 +1,22 @@
+"""
+2 smart players, can pick a number only from the ends, alt chance,
+how to maximize the win margin
+"""
+a = [7,3,2,5,8,4]
+m = len(a)
+dp = [[0 for x in range(m)]for y in range(m)]     
+def f(i,j):
+    if i>len(a) or j>len(a):
+        return 0
+    if i>j:
+        return 0
+    if dp[i][j]!=0:
+        return dp[i][j]
+    #player one selectes from the ith end
+    q1 = a[i]+ min(f(i+2,j),f(i+1,j-1))
+    #player one selects from the jth end
+    q2 = a[j]+min(f(i,j-2),f(i+1,j-1))
+    dp[i][j]=min(q1,q2)
+    return dp[i][j]
+f(0,len(a))
+print(dp[m-1][m-1])
--- SPOJ/Mixtures_SPOJ.py
+++ SPOJ/Mixtures_SPOJ.py
@@ -1,9 +1,9 @@
 nums = []
-dp = [[-1 for x in range(1000)]for y in range(1000)]
+dp = [[-1 for x in range(10)]for y in range(10)]
 import sys
 def custsum(s,e):
     res=0
-    for i in range(s,e):
+    for i in range(s,e+1):
             res+=nums[i]
             res%=100
     return res
@@ -15,15 +15,14 @@ def minSmoke(i,j):
     #if answer already completed
     if dp[i][j]!=-1:
         return dp[i][j]
-    
+    #rec case, break exp at every val of k
     dp[i][j]=sys.maxsize
-    for k in range(i,j):
-        dp[i][j]=min(dp[i][j],minSmoke(i,k)+minSmoke(k+1,j)+custsum(i,k)*custsum(k+1,j))
+    for k in range(i,j+1):
+        dp[i][j]=min(dp[i][j],minSmoke(i,k)+minSmoke(k+1,j)+(custsum(i,k)*custsum(k+1,j)))
 
     return dp[i][j]
 
 if __name__=="__main__":
     n=int(input())
-    for i in range(0,n):
-        nums.append(int(input()))
+    nums = [int(x) for x in input().split(" ")]
     print(minSmoke(0,n-1))
