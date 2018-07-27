029800769849fba12e824df0fd1bda20f52edd95
Satwik <satwik.mishra1995@gmail.com>
2018-07-27 21:05:19 +0530

    initial commit

--- SPOJ/Farida_SPOJ.py
+++ SPOJ/Farida_SPOJ.py
@@ -12,11 +12,17 @@ if __name__=="__main__":
         cases = int(input())
         i=1
         result = []
+        nums = []
+        n = []
+        j=0
         while i<=cases:
-                n = int(input())
-                nums = list(map(int, input().split(" ")))
-                count = collections.Counter(nums)
-                result.append(maxCoins(count,nums,n))
+                inp = int(input())
+                n.append(inp)
+                nums.append([int(x) for x in input().split(" ")])
                 i+=1
+        for a in n:
+                count = collections.Counter(nums[j])
+                result.append(maxCoins(count,nums[j],a))
+                j+=1
         for i in range(0,cases):
-                print("Case "+str(i+1)+":",result[i])
+                print("Case "+str(i+1)+": "+str(sresult[i]))
