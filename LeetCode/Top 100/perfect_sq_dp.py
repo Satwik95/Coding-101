0b0b08f00cc8c69b3ff44d1f904aa26a68d1055b
Satwik <satwik.mishra1995@gmail.com>
2019-02-08 18:56:40 -0500

    sq prob using bfs

--- LeetCode/Top 100/perfect_sq_dp.py
+++ LeetCode/Top 100/perfect_sq_dp.py
@@ -12,8 +12,23 @@ class Solution(object):
             if temp>n:
                 break
             sqs.append(temp)
-        dp = [i for i in range(n+1)]
+        """dp = [i for i in range(n+1)]
         for i in range(4,n+1):
             for j in sqs:
                 dp[i] = min(dp[i], dp[i-j]+1)
-        return dp[-1]
+        return dp[-1]"""
+        # using bfs
+        count = 0
+        frontier = [n]
+        while frontier:
+            count += 1 # for each level we are going down increment count
+            temp = set()
+            for u in frontier:
+                for v in sqs:
+                    if u==v:
+                        return count
+                    if u<v:
+                        break
+                    temp.add(u-v)
+            frontier = temp    
+        return count
