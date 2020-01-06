dc1bdb8dac2c3e78c25239f74f267cd86ac8d4f4
Satwik Mishra <satwik.mishra1995@gmail.com>
2020-01-06 15:08:47 -0500

    Update no_islands.py
    Updated the solution.

--- LeetCode/Top 100/no_islands.py
+++ LeetCode/Top 100/no_islands.py
@@ -4,32 +4,28 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
+
         def in_grid(x, y):
             return 0 <= x < len(grid) and 0 <= y < len(grid[0])
-        
+
         def neighbours(node):
-            print(node)
             p, q = node
-            dir = ((-1,0),(0,1),(1,0),(0,-1))
-            return [(x, y) for x, y in [(p+i,q+j) for i, j in dir] if in_grid(x, y)]
-            
+            dir = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+            return [(p + i, q + j) for i, j in dir if in_grid(p + i,  q + j)]
+
         def dfs(node):
-            self.visited.append(node)
+            visited.add(node)
             for v in neighbours(node):
-                if grid[v[0]][v[1]]==1:
+                if grid[v[0]][v[1]]== "1" and v not in visited:
                     dfs(v)
-                
+
         components = 0
-        self.visited = []
-        dfs((0,0))
+        visited = set()
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                node = (i,j)
-                if grid[i][j]=="1 and node not in self.visited:
+                node = (i, j)
+                if grid[i][j] == "1" and node not in visited:
                     components += 1
                     dfs(node)
-        print(self.visited)
+
         return components
-
-
-print(Solution().numIslands())
